f308ec55fd5b32e60f8a77c56587b1cd6030526f
Ryu GukHyeon <rgh4806@naver.com>
2020-07-23 17:24:31 +0900

    Base

---
+++ Blind_Server/Blind_Server/_Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Blind_Server
+{
+    class _Main
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        const int SW_HIDE = 0;
+
+        static void Main(string[] args)
+        {
+            var handl = GetConsoleWindow();
+            //ShowWindow(handl, SW_HIDE); //Console 창 숨기기
+        }
+    }
+}
